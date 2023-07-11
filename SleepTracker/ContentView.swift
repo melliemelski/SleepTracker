dffc61b6fb6599288abaeaa88a28b81d78558d52
Mel Sargent <melissa.sargent@hotmail.co.uk>
2023-07-11 22:13:40 +0100

    Initial Commit

---
+++ SleepTracker/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SleepTracker
+//
+//  Created by Melissa Sargent on 11/07/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SleepTracker/SleepTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SleepTrackerApp.swift
+//  SleepTracker
+//
+//  Created by Melissa Sargent on 11/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct SleepTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
